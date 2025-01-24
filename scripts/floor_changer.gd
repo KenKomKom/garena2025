3668f91d62ebdf54949661d7999658c682368624
KenKomKom <kenichikomala@gmail.com>
2025-01-24 19:10:14 +0700

    floor_changer done

--- scripts/floor_changer.gd
+++ scripts/floor_changer.gd
@@ -3,16 +3,33 @@ extends Node2D
 var p1_entered = false
 var p2_entered = false
 
-var player_triggerer = null
+var player1_triggerer = null
+var player2_triggerer = null
 @export var next_floor:Node2D
 
 func _process(delta):
-	if p1_entered and Input.is_action_just_pressed("leftp1") and Input.is_action_just_pressed("rightp1") or (p2_entered and Input.is_action_just_pressed("leftp2") and Input.is_action_just_pressed("rightp2")):
-		player_triggerer.set_move_to_target(next_floor)
-	
+	var p1_temp = Input.is_action_just_pressed("leftp1") and Input.is_action_just_pressed("rightp1")
+	var p2_temp = Input.is_action_just_pressed("leftp2") and Input.is_action_just_pressed("rightp2")
+	if (p1_entered and p1_temp) or \
+	(p2_entered and p2_temp):
+		if player1_triggerer and p1_temp:
+			player1_triggerer.set_move_to_target(next_floor)
+		if player2_triggerer and p2_temp:
+			player2_triggerer.set_move_to_target(next_floor)
 func _on_area_2d_body_entered(body):
-	pass # Replace with function body.
-
+	if body is Player:
+		if body.player_number==2:
+			p2_entered = true
+			player2_triggerer = body
+		elif body.player_number==1:
+			player1_triggerer = body
+			p1_entered = true
 
 func _on_area_2d_body_exited(body):
-	pass # Replace with function body.
+	if body is Player:
+		if body.player_number==2:
+			p2_entered = false
+			player2_triggerer = null
+		elif body.player_number==1:
+			player1_triggerer = null
+			p1_entered = false
