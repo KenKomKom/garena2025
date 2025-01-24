81f080abedb0d1d436ebef44ea53b36c1c460743
Fredo Melvern Tanzil <55704134+Frmeta@users.noreply.github.com>
2025-01-24 22:52:02 +0700

    yes

--- scripts/ship.gd
+++ scripts/ship.gd
@@ -9,11 +9,11 @@ var live_tank :=3
 
 func _ready():
 	GameManager.connect("tank_minus", air_minus)
-	pass # Replace with function body.
+	
 
 
 func _process(delta):
-	pass
+	$Label.text = "oxygen: " + str(oxygen_level) + "\npressure: " + str(pressure_level)
 
 func air_minus():
 	live_tank-=1
