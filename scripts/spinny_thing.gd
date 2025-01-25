d380cf917050aa5fd644afd2887cb3de9bb5cfc5
Fredo Melvern Tanzil <55704134+Frmeta@users.noreply.github.com>
2025-01-25 13:26:09 +0700

    upgrade spinny thing

--- scripts/spinny_thing.gd
+++ scripts/spinny_thing.gd
@@ -1,12 +1,17 @@
 extends Node2D
 
-var textures = [preload("res://assets/ken1.png"),preload("res://assets/ken2.png"),\
-preload("res://assets/ken3.png"),preload("res://assets/ken4.png")]
-var sudut = [[-60,-34],[-22,-2],[6,30],[38,65]]
+var textures = [
+	preload("res://assets/var1.png"),\
+	preload("res://assets/var2.png"),\
+	preload("res://assets/var3.png"),\
+	preload("res://assets/var4.png")
+]
+var sudut = [[46,76],[-72,-40],[-30,-1],[0,30]]
+const TOLERANCE = 5
 var can_take_input = false
 var dir = 1
-var SPEED = 70
-@onready var segitiga = $Sprite2D
+var SPEED = 200
+@onready var segitiga = $Segitiga
 
 var player_id : int
 var sudut_check 
@@ -26,11 +31,16 @@ func _process(delta):
 	if can_take_input:
 		segitiga.rotation_degrees += SPEED*delta*dir
 		if segitiga.rotation_degrees>90:
-			dir = -1
+			dir = -60 * delta
 		elif segitiga.rotation_degrees<-90:
-			dir = 1
-		if Input.is_action_just_pressed("leftp"+str(player_id)) and Input.is_action_just_pressed("rightp"+str(player_id)):
-			if segitiga.rotation_degrees>=sudut_check[0] and segitiga.rotation_degrees<=sudut_check[1]:
+			dir = 60 * delta
+		
+		if str(player_id) == "1" and GameManager.get_leftp1() and GameManager.get_rightp1() or \
+			str(player_id) == "2" and GameManager.get_leftp2() and GameManager.get_rightp2():
+			can_take_input = false
+			await get_tree().create_timer(0.5).timeout
+			can_take_input = true
+			if segitiga.rotation_degrees>=sudut_check[0]-TOLERANCE and segitiga.rotation_degrees<=sudut_check[1]+TOLERANCE:
 				can_take_input = false
 				visible = false
 				emit_signal("success")
