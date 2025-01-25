6d11b0b8d5ddf8bbf4169211839f9c9701892cb7
Fredo Melvern Tanzil <55704134+Frmeta@users.noreply.github.com>
2025-01-25 18:06:55 +0700

    progress on fish

--- scripts/ship.gd
+++ scripts/ship.gd
@@ -11,11 +11,15 @@ var can_meltdown = false
 var can_attacked_fish = false
 
 var status_lampu = false
-var is_aggresive_fish_in_death_area = false
+var is_aggresive_fish_in_death_area = 0
 
 @onready var list_of_available_bocor_spot := $Node2D
 
 const sea_color = ["#9bf1de","#4174a7","#2c3884","#231760","#100838"]
+
+@onready var fish_ok = preload("res://scenes/fish_Ok.tscn")
+@onready var fish_odd = preload("res://scenes/fish_Odd.tscn")
+@onready var fish_big = preload("res://scenes/fish_Big.tscn")
 
 
 func _ready():
@@ -40,9 +44,9 @@ func bocor_mulai(zone):
 		GameManager.ZONE.MESOPELAGIC:
 			# Level 2
 			can_bocor = true
-			can_meltdown = false
-			can_attacked_fish = false
 			$bocor_timer.start()
+			can_attacked_fish = false
+			$attacked_fish_timer.start()
 			var p = %manual as Manual
 			p.add_text(p.EVENTS.LEAKS)
 			p.add_text(p.EVENTS.PREDATOR_FISH)
@@ -50,27 +54,33 @@ func bocor_mulai(zone):
 		GameManager.ZONE.BATHYPELAGIC:
 			# Level 3
 			can_bocor = true
+			$bocor_timer.start()
 			can_meltdown = true
-			can_attacked_fish = true
 			$meltdown_timer.start()
+			can_attacked_fish = true
+			$attacked_fish_timer.start()
 			var p = %manual as Manual
 			p.add_text(p.EVENTS.NUCLEAR_MELTDOWN)
 			p.animate_in()
 		GameManager.ZONE.ABYSSOPELAGIC:
 			# Level 4
 			can_bocor = true
+			$bocor_timer.start()
 			can_meltdown = true
-			can_attacked_fish = true
 			$meltdown_timer.start()
+			can_attacked_fish = true
+			$attacked_fish_timer.start()
 			var p = %manual as Manual
 			p.add_text2(GameManager.zone_now)
 			p.animate_in()
 		GameManager.ZONE.HADAL:
 			# Level 5
 			can_bocor = true
+			$bocor_timer.start()
 			can_meltdown = true
-			can_attacked_fish = true
 			$meltdown_timer.start()
+			can_attacked_fish = true
+			$attacked_fish_timer.start()
 			var p = %manual as Manual
 			p.add_text2(GameManager.zone_now)
 			p.animate_in()
@@ -122,18 +132,29 @@ func _process(delta):
 		_on_attacked_fish_timer_timeout()
 		
 	# check if fish is in death area
-	if is_aggresive_fish_in_death_area and status_lampu:
+	if is_aggresive_fish_in_death_area>0 and status_lampu:
 		GameManager.emit_signal("game_over",GameManager.DEATH_REASON.BIG_FISH)
 
 func _on_attacked_fish_timer_timeout():
 	if can_attacked_fish:
-		$attacked_fish_timer.wait_time = randi_range(30,40) - GameManager.zone_now*2
+		$attacked_fish_timer.wait_time = randi_range(20,30) - GameManager.zone_now*2
 		
 		var tween = get_tree().create_tween()
 		const mid = Vector2(960, 540)
 		const range_anim = 3000
-		const duration = 10
+		const duration = 20
 		var rand_angle = randf_range(-PI/4, PI/4)
+		
+		# instantiate new fish
+		# {EPIPELAGIC, MESOPELAGIC, BATHYPELAGIC, ABYSSOPELAGIC, HADAL}
+		var a
+		if GameManager.zone_now == GameManager.ZONE.BATHYPELAGIC:
+			a = fish_ok.instantiate()
+		elif GameManager.zone_now == GameManager.ZONE.ABYSSOPELAGIC:
+			a = fish_odd.instantiate()
+		else:
+			a = fish_big.instantiate()
+		add_child(a)
 			
 		if GameManager.zone_now == GameManager.ZONE.ABYSSOPELAGIC:
 			# pengecualian abyssopelagic, aggresive fish swim dari bawah ke atas
@@ -142,16 +163,16 @@ func _on_attacked_fish_timer_timeout():
 		else:
 			if randi() % 2 == 1:
 				# dari ke kiri mau ke kanan
-				$Fishes/Fish.rotation = rand_angle
-				$Fishes/Fish/Sprite2D.flip_h = false
+				a.rotation = rand_angle
+				a.get_node("Sprite2D").flip_h = false
 			else:
 				# dari kanan mau ke kiri
-				$Fishes/Fish.rotation = rand_angle
-				$Fishes/Fish/Sprite2D.flip_h = true
+				a.rotation = rand_angle
+				a.get_node("Sprite2D").flip_h = true
 				rand_angle += PI
 				
-		$Fishes/Fish.position = mid + Vector2(-cos(rand_angle), -sin(rand_angle)) * range_anim
+		a.position = mid + Vector2(-cos(rand_angle), -sin(rand_angle)) * range_anim
 			
-		tween.tween_property($Fishes/Fish, "position", Vector2(mid - Vector2(-cos(rand_angle), -sin(rand_angle)) * range_anim), duration).set_trans(Tween.TRANS_LINEAR)
-		#tween.tween_callback($Fishes/Fish.queue_free)
+		tween.tween_property(a, "position", Vector2(mid - Vector2(-cos(rand_angle), -sin(rand_angle)) * range_anim), duration).set_trans(Tween.TRANS_LINEAR)
+		tween.tween_callback(a.queue_free)
 		
