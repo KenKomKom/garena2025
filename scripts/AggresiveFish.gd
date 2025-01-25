b3071fa284ce9e3d40c1cdbc22c06ef3b5931abb
Fredo Melvern Tanzil <55704134+Frmeta@users.noreply.github.com>
2025-01-25 17:20:37 +0700

    wow fish big

--- scripts/AggresiveFish.gd
+++ scripts/AggresiveFish.gd
@@ -3,8 +3,6 @@ extends Area2D
 func _on_area_entered(area):
 	if area.name == "TheDeathArea":
 		$"../..".is_aggresive_fish_in_death_area = true
-		
-
 
 func _on_area_exited(area):
 	if area.name == "TheDeathArea":
